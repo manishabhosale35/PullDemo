6aa6a1d07a6e777699754508d47785861b9641cd
manishabhosale35 <manishabhosale35@gmail.com>
2025-02-11 00:01:36 +0530

    First commit

---
+++ SwapingNumber.java
@@ -0,0 +1,29 @@
+package com.wipro.mypractice;
+
+public class SwapingNumber {
+
+	public static void main(String[] args) {
+//		int a = 10;
+//		int b = 20;
+//		System.out.println("--Before swap --");
+//		System.out.println("First number = " + a);
+//		System.out.println("Second number = " + b);
+//
+//		a = a - b;
+//		b = a + b;
+//		a = b - a;
+//		
+//		System.out.println("--After swap --");
+//		System.out.println("First number = " + a);
+//		System.out.println("Second number = " + b);
+//
+//	}
+
+	
+	TestExample te = new TestExample ("AFFE001", 5000);
+	te.deposit(2000);
+	te.withdraw(1000);
+	te.applyInterest();
+
+}
+}
